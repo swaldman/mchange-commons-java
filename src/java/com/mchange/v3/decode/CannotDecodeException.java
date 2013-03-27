218d20ac784944ee922a0a23593f7e524afe0466
Steve Waldman <swaldman@mchange.com>
2013-03-27 11:19:52 +0000

    Fixed stupidly incomplete CannotDecodeException.

--- src/java/com/mchange/v3/decode/CannotDecodeException.java
+++ src/java/com/mchange/v3/decode/CannotDecodeException.java
@@ -37,10 +37,16 @@ package com.mchange.v3.decode;
 
 public class CannotDecodeException extends Exception
 {
-    CannotDecodeException( String message )
+    public CannotDecodeException( String message, Throwable t )
+    { super( message, t ); }
+
+    public CannotDecodeException( String message )
     { super( message ); }
 
-    CannotDecodeException()
+    public CannotDecodeException( Throwable t )
+    { super( t ); }
+
+    public CannotDecodeException()
     { super(); }
 }
 
