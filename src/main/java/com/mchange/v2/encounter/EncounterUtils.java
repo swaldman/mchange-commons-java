a732b2871123d036dcfbdb1a9e2954fae7bcc0c3
Steve Waldman <swaldman@mchange.com>
2013-05-23 00:23:53 -0700

    Added a method to wrap an EncounterCounter behind a synchronization barrier.

--- src/main/java/com/mchange/v2/encounter/EncounterUtils.java
+++ src/main/java/com/mchange/v2/encounter/EncounterUtils.java
@@ -45,6 +45,20 @@ public final class EncounterUtils
     public static EncounterCounter createWeak( Identicator id )
     { return new GenericEncounterCounter( new IdWeakHashMap( id ) ); }
 
+    /**
+     * returns the inner EncounterCounter wrapped so that all
+     * method calls are effectively synchronized.
+     */
+    public static EncounterCounter syncWrap( final EncounterCounter inner )
+    {
+	return new EncounterCounter()
+	{
+	    public synchronized long encounter(Object o) { return inner.encounter(o); }
+	    public synchronized long reset(Object o) { return inner.reset(o); }
+	    public synchronized void resetAll() { inner.resetAll(); }
+	};
+    }
+
     private EncounterUtils()
     {}
 }
