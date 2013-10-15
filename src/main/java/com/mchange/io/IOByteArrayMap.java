b0cc3e22cff8d4d003f0541d7d4e417bb6604556
Steve Waldman <swaldman@mchange.com>
2013-10-15 00:47:45 -0700

    Removed some vestigial GPL license headers from the late 1990s. Many thanks to bsev on github from unearthing these and calling attention to the issue.

--- src/main/java/com/mchange/io/IOByteArrayMap.java
+++ src/main/java/com/mchange/io/IOByteArrayMap.java
@@ -33,30 +33,6 @@
  * 
  */
 
-/*
- *  IOByteArrayMap.java
- *
- *  A map of byte[] to byte[] that may be disk or network bound.
- *  This interface is not intended to be expressed by implementations
- *  supporting duplicate keys.
- *
- *  (C) Copyright 1998, Machinery For Change, Inc.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 package com.mchange.io;
 
 import java.io.*;
--- src/main/java/com/mchange/io/IOEnumeration.java
+++ src/main/java/com/mchange/io/IOEnumeration.java
@@ -33,28 +33,6 @@
  * 
  */
 
-/*
- *  IOEnumeration.java
- *  An enumeration interface that permits IOExceptions,
- *  intended for disk and network-bound collection.
- *
- *  (C) Copyright 1998, Machinery For Change, Inc.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 package com.mchange.io;
 
 import java.io.*;
