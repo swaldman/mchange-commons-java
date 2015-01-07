258fc38c71394e7f3d84937cf584f16b20a23c04
Steve Waldman <swaldman@mchange.com>
2015-01-07 07:44:21 -0500

    Added toLowercaseHexAscii(...) and unsignedPromote(...) to ByteUtils.

--- src/main/java/com/mchange/lang/ByteUtils.java
+++ src/main/java/com/mchange/lang/ByteUtils.java
@@ -43,13 +43,26 @@ public final class ByteUtils
 {
   public final static short UNSIGNED_MAX_VALUE = (Byte.MAX_VALUE * 2) + 1;
 
+  /**
+   * @deprecated prefer unsignedPromote(...)
+   */  
   public static short toUnsigned(byte b)
     {return (short) (b < 0 ? (UNSIGNED_MAX_VALUE + 1) + b : b);}
+
+  public static int unsignedPromote(byte b)
+    { return b & 0xff; }
 
   public static String toHexAscii(byte b)
     {
       StringWriter sw = new StringWriter(2);
       addHexAscii(b, sw);
+      return sw.toString();
+    }
+
+  public static String toLowercaseHexAscii(byte b)
+    {
+      StringWriter sw = new StringWriter(2);
+      addLowercaseHexAscii(b, sw);
       return sw.toString();
     }
 
@@ -59,6 +72,15 @@ public final class ByteUtils
       StringWriter sw = new StringWriter(len * 2);
       for (int i = 0; i < len; ++i)
 	addHexAscii(bytes[i], sw);
+      return sw.toString();
+    }
+
+  public static String toLowercaseHexAscii(byte[] bytes)
+    {
+      int len = bytes.length;
+      StringWriter sw = new StringWriter(len * 2);
+      for (int i = 0; i < len; ++i)
+	addLowercaseHexAscii(bytes[i], sw);
       return sw.toString();
     }
 
@@ -86,13 +108,22 @@ public final class ByteUtils
     }
 
   static void addHexAscii(byte b, StringWriter sw)
-    {
-      short ub = toUnsigned(b);
+  {
+      int ub = unsignedPromote(b);
       int h1 = ub / 16;
       int h2 = ub % 16;
       sw.write(toHexDigit(h1));
       sw.write(toHexDigit(h2));
-    }
+  }
+
+  static void addLowercaseHexAscii(byte b, StringWriter sw )
+  {
+      int ub = unsignedPromote(b);
+      int h1 = ub / 16;
+      int h2 = ub % 16;
+      sw.write(toLowercaseHexDigit(h1));
+      sw.write(toLowercaseHexDigit(h2));
+  }
 
   private static int fromHexDigit(int c) throws NumberFormatException
     {
@@ -117,6 +148,19 @@ public final class ByteUtils
 	//System.err.println(h + ": " + out);
 	return out;
     }
+
+
+  /* note: we do no arg. checking, because     */
+  /* we only ever call this from addHexAscii() */
+  /* above, and we are sure the args are okay  */
+  private static char toLowercaseHexDigit(int h)
+    {
+	char out;
+	if (h <= 9) out = (char) (h + 0x30);
+	else out = (char) (h + 0x57);
+	return out;
+    }
+
 
   private ByteUtils()
     {}
