c10ad155f6ccca56d9afca4d9180f30663a83b82
Steve Waldman <swaldman@mchange.com>
2017-06-14 16:21:01 -0700

    Add ByteBufferUtils

---
+++ src/main/java/com/mchange/v3/nio/ByteBufferUtils.java
@@ -0,0 +1,20 @@
+package com.mchange.v3.nio;
+
+import java.nio.ByteBuffer;
+
+public final class ByteBufferUtils
+{
+    public byte[] newArray( ByteBuffer bb )
+    {
+	if ( bb.hasArray() ) {
+	    return (byte[]) bb.array().clone();
+	} else {
+	    byte[] out = new byte[bb.remaining()];
+	    bb.get(out);
+	    return out;
+	}
+    }
+
+    private ByteBufferUtils()
+    {}
+}
