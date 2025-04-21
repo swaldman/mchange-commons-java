e1b91da0ce584e526e163a55ad52f78a1384c878
Steve Waldman <swaldman@mchange.com>
2025-04-21 14:05:03 -0400

    Add documentation emphasizing that reporting is optional.

--- src/main/java/com/mchange/v2/async/ThreadPoolReportingAsynchronousRunner.java
+++ src/main/java/com/mchange/v2/async/ThreadPoolReportingAsynchronousRunner.java
@@ -35,6 +35,14 @@
 
 package com.mchange.v2.async;
 
+/**
+ *  Note: All "reporting" values below are **optional**.
+ *
+ *  Implementations may return -1 for int values, empty string or (perhaps, be prepared) null for String values.
+ *
+ *  These methods are intended to provide optional reporting to users, for dashboards and debugging,
+ *  They cannot be relied upon.
+ */
 public interface ThreadPoolReportingAsynchronousRunner extends AsynchronousRunner
 {
     public int getThreadCount();
