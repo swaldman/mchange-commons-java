8278108fe6e5acfb9534c0ebe35fee80e8bedccd
Steve Waldman <swaldman@mchange.com>
2026-02-14 21:19:44 -0300

    Choose a slightly better better SecurityConfigKey name.

--- src/main/java/com/mchange/v2/naming/SecurityConfigKey.java
+++ src/main/java/com/mchange/v2/naming/SecurityConfigKey.java
@@ -5,7 +5,7 @@ public class SecurityConfigKey {
     public final static String SUPPORT_REFERENCE_REMOTE_FACTORY_CLASS_LOCATION = "com.mchange.v2.naming.supportReferenceRemoteFactoryClassLocation";
 
     // not used directly here, but intended for use by applications (e.g. c3p0)
-    public final static String PERMIT_NONLOCAL_JNDI_URLS                       = "com.mchange.v2.naming.permitNonlocalJndiUrls";
+    public final static String PERMIT_NONLOCAL_JNDI_NAMES                       = "com.mchange.v2.naming.permitNonlocalJndiNames";
 
     private SecurityConfigKey() {}
 }
