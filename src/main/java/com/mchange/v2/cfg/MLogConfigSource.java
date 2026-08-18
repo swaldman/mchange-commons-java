9d14d7a1e37174cc5f685765135aa1dfb71829fe
Steve Waldman <swaldman@mchange.com>
2026-08-18 19:18:11 -0300

    Add @Deprecated annotation to a method deprecated only within the javadoc. (Thanks Claude!)

--- src/main/java/com/mchange/v2/cfg/MLogConfigSource.java
+++ src/main/java/com/mchange/v2/cfg/MLogConfigSource.java
@@ -13,6 +13,7 @@ public final class MLogConfigSource
     /**
      * @deprecated The vmConfig APIs are confusing. Use readUncachedClassloaderResourceConfig(...)
      */
+    @Deprecated
     public static MultiPropertiesConfig readVmConfig(String[] defaultResources, String[] preemptingResources, List delayedLogItemsOut)
     { return ConfigUtils.readVmConfig( defaultResources, preemptingResources, delayedLogItemsOut); }
 
